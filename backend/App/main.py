9b0abcf9223a7e39652f93b831478c132db2dbcb
Sahelyn-1006 <sahelnouri05@gmail.com>
2025-11-17 16:46:55 -0500

    created the middleware and mounted the api routes under the /api prefix

--- backend/App/main.py
+++ backend/App/main.py
@@ -1,17 +1,22 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from .routes import router as api_router
 
 app = FastAPI()
 
-# Allow frontend to talk to backend
+# Middleware looks at whether to block or allow requests from different origins, so not everyone has access to my backend APIS
+# Added CORS middleware
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Later restrict to your frontend domain
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    CORSMiddleware, #CORS middleware, Cross Origin Resource Sharing, meaninng it deals with CORS requests from the frontend, ie certain origins are not allowed to call the backend unless the CORS is configured to allow it
+    allow_origins=["*"], # allows all origins to call backend APIS (for development, will be changed later to only allow frontend origin)
+    allow_credentials=True, #allows cookies/ auth headers to be sent from the frontend
+    allow_methods=["*"], #allows all HTTP methods to be sent from those origins
+    allow_headers=["*"], #allows any custom headers like Authorization, Content_Type etc in reqs
 )
 
 @app.get("/")
 def root():
-    return {"message": "Backend is running 🚀"}
+    return {"message": "Backend is running"}
+
+#mounted my api routes (generate and history endpoints) under the /api prefix
+app.include_router(api_router, prefix= "/api")
