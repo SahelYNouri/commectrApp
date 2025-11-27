fa8e33b7911e3d6a7c0f94b88fc2090678d5acc9
Sahelyn-1006 <sahelnouri05@gmail.com>
2025-11-27 18:49:45 -0500

    adjusted the origin for my vercel app

--- backend/App/main.py
+++ backend/App/main.py
@@ -4,11 +4,16 @@ from .routes import router as api_router
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",                 # Vite dev
+    "https://commectr-app.vercel.app/",    # replace with your actual Vercel URL
+]
+
 # Middleware looks at whether to block or allow requests from different origins, so not everyone has access to my backend APIS
 # Added CORS middleware
 app.add_middleware(
     CORSMiddleware, #CORS middleware, Cross Origin Resource Sharing, meaninng it deals with CORS requests from the frontend, ie certain origins are not allowed to call the backend unless the CORS is configured to allow it
-    allow_origins=["*"], # allows all origins to call backend APIS (for development, will be changed later to only allow frontend origin)
+    allow_origins=origins, # allows all origins to call backend APIS (for development, will be changed later to only allow frontend origin)
     allow_credentials=True, #allows cookies/ auth headers to be sent from the frontend
     allow_methods=["*"], #allows all HTTP methods to be sent from those origins
     allow_headers=["*"], #allows any custom headers like Authorization, Content_Type etc in reqs
