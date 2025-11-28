529c851a4f130c528b6e6f5bb5cea502437f9190
Sahelyn-1006 <sahelnouri05@gmail.com>
2025-11-28 15:06:35 -0500

    fixed the origin name for frontend deployment.

--- backend/App/main.py
+++ backend/App/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 origins = [
     "http://localhost:5173",                 # Vite dev
-    "https://commectr-app.vercel.app/",    # replace with your actual Vercel URL
+    "https://commectr-app.vercel.app",    #vercel url
 ]
 
 # Middleware looks at whether to block or allow requests from different origins, so not everyone has access to my backend APIS
@@ -14,7 +14,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware, #CORS middleware, Cross Origin Resource Sharing, meaninng it deals with CORS requests from the frontend, ie certain origins are not allowed to call the backend unless the CORS is configured to allow it
     allow_origins=origins, # allows all origins to call backend APIS (for development, will be changed later to only allow frontend origin)
-    allow_credentials=True, #allows cookies/ auth headers to be sent from the frontend
+    allow_credentials=False, #doesnt allow cookies/ auth headers to be sent from the frontend
     allow_methods=["*"], #allows all HTTP methods to be sent from those origins
     allow_headers=["*"], #allows any custom headers like Authorization, Content_Type etc in reqs
 )
